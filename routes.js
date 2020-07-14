a68be5e8126be67f15e7cb37ffdd15f212de34bf
edimilsonbraz <edimilson.gt8@gmail.com>
2020-07-13 22:03:04 -0300

    add method put and delete

--- routes.js
+++ routes.js
@@ -19,6 +19,10 @@ routes.get('/teachers/:id', teachers.show)
 routes.get('/teachers/:id/edit', teachers.edit)
 
 routes.post("/teachers", teachers.post) 
+
+routes.put("/teachers", teachers.update)
+
+routes.delete("/teachers", teachers.delete)
     
 routes.get('/students', function(req, res) {
     return res.send("students")
--- teachers.js
+++ teachers.js
@@ -82,3 +82,48 @@ exports.edit = function(req, res) {
     return res.render("teachers/edit", { teacher })
 }
 
+//Update
+exports.update = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+        if(id == teacher.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundTeacher) return res.send("Teacher not Found")
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error / method put")
+
+        return res.redirect(`/teachers/${id}`)
+    })
+
+}
+
+//Delete
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect("/teachers")
+    })
+}
